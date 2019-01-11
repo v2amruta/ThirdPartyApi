e0c7c7a3efacaed53d848e87c10148d5b9cc8018
Amruta Shinde <amruta.shinde@v2solutions.com>
2019-01-11 12:52:53 +0530

    Replaced name of blogs's model properties.

--- Controllers/BlogsController.cs
+++ Controllers/BlogsController.cs
@@ -18,9 +18,9 @@ namespace RWRAP_API.ThirdPartyApi.Controller
         public object Blogs()
         {
             List<Blogs> blogs = new List<Blogs>();
-            blogs.Add(new Blogs() { Name = "MOBILE", path = "https://www.v2solutions.com/category/mobile/" });
-            blogs.Add(new Blogs() { Name = "QA", path = "https://www.v2solutions.com/category/qa/" });
-            blogs.Add(new Blogs() { Name = "TECHNOLOGY", path = "https://www.v2solutions.com/category/technology/" });
+            blogs.Add(new Blogs() { Key = "MOBILE", Value = "https://www.v2solutions.com/category/mobile/" });
+            blogs.Add(new Blogs() { Key = "QA", Value = "https://www.v2solutions.com/category/qa/" });
+            blogs.Add(new Blogs() { Key = "TECHNOLOGY", Value = "https://www.v2solutions.com/category/technology/" });
 
             return blogs;
         }
